c52fbc0e9bf3d34a3d890327dfbc83b4be939e4f
Ravindu Jayaweera <ravindujayaweera99@gmail.com>
2024-05-25 18:39:42 +0530

    config vercel

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import goalRoutes from "./routes/goalRoutes.js";
 import errorHandler from "./middleware/errorMiddleware.js";
-import colors from 'colors';
+import colors from "colors";
 import connectDB from "./config/db.js";
-import userRoutes from "./routes/userRoutes.js"
+import userRoutes from "./routes/userRoutes.js";
+import path from "path";
 
 const PORT = process.env.PORT;
 
@@ -16,6 +17,19 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", goalRoutes);
 app.use("/api/users", userRoutes);
+
+//serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontned", "build", "index.html")
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to Production Mode!'))
+}
 
 app.use(errorHandler);
 
